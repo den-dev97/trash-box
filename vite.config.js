10bacdfc0b0891577e22b3e2db99f1c40f6c4674
Denis Khaustov <khausden@gmail.com>
2022-01-31 20:28:38 +1000

    -initial commit

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/trash-box/',
   plugins: [vue()],
 })
